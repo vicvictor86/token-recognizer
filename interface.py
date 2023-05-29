650aca7909498264fb3c8544a9e64bb7d09aa4ba
Amarildo Junior <ajunior283@outlook.com>
2023-05-28 21:34:36 -0300

    Adding empty html file warning and a popup to show the tokens

--- interface.py
+++ interface.py
@@ -1,6 +1,7 @@
 import tkinter as tk
-from tkinter import filedialog
 from tokenRecognizer import getHTMLTokens
+from tkinter import filedialog
+from tkinter import messagebox
 
 
 def open_file():
@@ -12,27 +13,51 @@ def open_file():
     else:
         return ""
 
+def popup_message(title, message, icon="info"):
+    messagebox.showinfo(title, message, icon=icon)
 
 def show_code():
     html_code = open_file()
     valid_html_tokens, all_tokens = getHTMLTokens(html_code)
 
-    text_widget.delete('1.0', tk.END)
-    text_widget.insert(tk.END, all_tokens)
+    # text_widget.delete('1.0', tk.END)
+    # text_widget.insert(tk.END, all_tokens)
+    
+    if html_code == "":
+        popup_message("HTML Tokens", "Nenhum arquivo foi selecionado ou o arquivo está vazio.", "warning")
+    else:
+        popup_message("HTML Tokens", all_tokens)
 
-    print(valid_html_tokens, all_tokens)
+    # print(valid_html_tokens, all_tokens)
+
+def change_cursor(event):
+    open_button.configure(cursor="hand2")
 
 
 root = tk.Tk()
 root.title("Visualizador de Código HTML")
 
-frame = tk.Frame(root)
-frame.pack(pady=20)
 
-open_button = tk.Button(frame, text="Abrir Arquivo", command=show_code)
-open_button.pack(side=tk.LEFT)
+root.geometry("800x600")
+root.state("zoomed")
+root.configure(bg="#1a1a1a")
 
-text_widget = tk.Text(root, width=80, height=30)
-text_widget.pack(pady=10)
+frame = tk.Frame(root, bg="#1a1a1a")
+frame.pack(pady=50)
+
+open_button = tk.Button(
+        frame,
+        text="Abrir Arquivo",
+        font=("Arial", 14),
+        bg="#4682B4",
+        fg="white",
+        relief="flat",
+        padx=20,
+        pady=10, command=show_code)
+open_button.pack(side=tk.BOTTOM)
+open_button.bind("<Enter>", change_cursor)
+
+# text_widget = tk.Text(root, width=80, height=30)
+# text_widget.pack(pady=10)
 
 root.mainloop()
